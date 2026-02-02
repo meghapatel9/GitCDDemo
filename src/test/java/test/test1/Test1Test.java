ce7e5cc98187b96e2b60faa83a41491bd8afd38c
meghapatel9 <46826564+meghapatel9@users.noreply.github.com>
2026-02-02 12:54:04 -0500

    Update Test1Test.java

--- src/test/java/test/test1/Test1Test.java
+++ src/test/java/test/test1/Test1Test.java
@@ -25,7 +25,7 @@ public class Test1Test {
     public void testGreet() {
         System.out.println("greet");
         Test1 instance = new Test1();
-        String expResult = "Hello";
+        String expResult = "hello";
         String result = instance.greet();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
@@ -43,5 +43,6 @@ public class Test1Test {
     }
     
 }
+
 
 
